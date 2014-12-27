7599ae05e9989e6e7b209c307f5304b35f87e801
benoitphilippe <fornoville.benoitphilippe@gmail.com>
2014-12-27 19:55:50 +0100

    oude CFGCYKCNF in andere map + formatter aangepast

--- CNF_CFG_CYK_FINAL/Formatter.cpp
+++ CNF_CFG_CYK_FINAL/Formatter.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "Formatter.h"
+#include <fstream>
 
 using std::endl;
 using std::cout;
@@ -16,7 +17,6 @@ Formatter::Formatter(string fileName)
 	OpenFileAndremoveSpaces(fileName);//result in s1
 	applyTransformation();//reads s1 and puts result in s2
 
-	cout << s2 << endl;
 }
 
 Formatter::~Formatter() {
@@ -24,6 +24,15 @@ Formatter::~Formatter() {
 }
 
 string Formatter::getFormatedVersion() const{
+	/*std::ofstream file;
+	file.open("form.txt");
+	for (int i=0; i<s2.size(); ++i){
+		file << s2[i];
+		if (s2[i]=='>' && i+2<s2.size() && s2[i+2]=='<'){
+			file << endl;
+		}
+	}
+	file.close();*/
 	return s2;
 }
 
@@ -63,22 +72,31 @@ void Formatter::applyTransformation(){
 	//loop over all the chars of the input string s1
 	//and create a string s2 that has ',' on the right places
 
+	bool inSpecialCase{false};
+	//don't split <?xml ... > and <!DOCTYPE ... >
+
 	bool inString{false};//check if we had a '"'
 	bool addKomma{false};//if we had '>'we have to add a ',' to s2
 	int brackets{0};
 
-	for (auto& ch : s1){
-		switch (ch){
+	for (int i=0; i<s1.size(); ++i){
+		switch (s1[i]){
 		case '<':
+			if (i+1<s1.size() && (s1[i+1]=='?' || s1[i+1]=='!') )
+				inSpecialCase = true;
 			brackets++;
 			break;
 		case '>':
+			if ( inSpecialCase )
+				inSpecialCase = false;
 			brackets--;
 			addKomma = true;
 			break;
 		case '"':
 			if (inString){
 				s2 += ',';
+				if (inSpecialCase)
+					s2.pop_back();
 				inString = false;
 			}
 			else inString = true;
@@ -86,16 +104,22 @@ void Formatter::applyTransformation(){
 		default:
 			if ( inString ){
 				s2 += ',';
+				if (inSpecialCase)
+					s2.pop_back();
 			}
 		}
-		s2 += ch;
+		s2 += s1[i];
 		if (addKomma){
 			addKomma = false;
 			s2 += ',';
+			if (inSpecialCase)
+				s2.pop_back();
 		}
 		else if ( brackets == 0){
 			s2 += ',';
+			if ( inSpecialCase )
+				s2.pop_back();
 		}
 	}
-
+	s2.pop_back();//remove the last char (which should be a ',')
 }
