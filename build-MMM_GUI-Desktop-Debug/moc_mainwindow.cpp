bfb32e0b4a48b29518862de0b67bc3bfa5a0cc7c
Rafael De Smet <rafael.de.smet@gmail.com>
2015-01-20 15:01:26 +0100

    e

---
+++ build-MMM_GUI-Desktop-Debug/moc_mainwindow.cpp
@@ -0,0 +1,137 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.2.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../Fase 2/MMM_GUI/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.2.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_mainWindow_t {
+    QByteArrayData data[9];
+    char stringdata[182];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    offsetof(qt_meta_stringdata_mainWindow_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData) \
+    )
+static const qt_meta_stringdata_mainWindow_t qt_meta_stringdata_mainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10),
+QT_MOC_LITERAL(1, 11, 23),
+QT_MOC_LITERAL(2, 35, 0),
+QT_MOC_LITERAL(3, 36, 25),
+QT_MOC_LITERAL(4, 62, 20),
+QT_MOC_LITERAL(5, 83, 20),
+QT_MOC_LITERAL(6, 104, 21),
+QT_MOC_LITERAL(7, 126, 26),
+QT_MOC_LITERAL(8, 153, 27)
+    },
+    "mainWindow\0on_browseButton_clicked\0\0"
+    "on_MusicXMLButton_clicked\0"
+    "on_MEIButton_clicked\0on_CSVButton_clicked\0"
+    "on_MIDIButton_clicked\0on_MuseScoreButton_clicked\0"
+    "on_MuseScoreButton_released\0"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_mainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       7,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   49,    2, 0x08,
+       3,    0,   50,    2, 0x08,
+       4,    0,   51,    2, 0x08,
+       5,    0,   52,    2, 0x08,
+       6,    0,   53,    2, 0x08,
+       7,    0,   54,    2, 0x08,
+       8,    0,   55,    2, 0x08,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void mainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        mainWindow *_t = static_cast<mainWindow *>(_o);
+        switch (_id) {
+        case 0: _t->on_browseButton_clicked(); break;
+        case 1: _t->on_MusicXMLButton_clicked(); break;
+        case 2: _t->on_MEIButton_clicked(); break;
+        case 3: _t->on_CSVButton_clicked(); break;
+        case 4: _t->on_MIDIButton_clicked(); break;
+        case 5: _t->on_MuseScoreButton_clicked(); break;
+        case 6: _t->on_MuseScoreButton_released(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObject mainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_mainWindow.data,
+      qt_meta_data_mainWindow,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *mainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *mainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_mainWindow.stringdata))
+        return static_cast<void*>(const_cast< mainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int mainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 7)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 7;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 7)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 7;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
