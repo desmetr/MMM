e9c1d3de9849088d57ea7738fc5a8757b2c41fd3
Rafael De Smet <rafael.de.smet@gmail.com>
2015-01-20 11:19:04 +0100

    s

--- build-MMM_GUI-Desktop-Debug/moc_mainwindow.cpp
+++ build-MMM_GUI-Desktop-Debug/moc_mainwindow.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_mainWindow_t {
-    QByteArrayData data[7];
-    char stringdata[127];
+    QByteArrayData data[9];
+    char stringdata[182];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -35,12 +35,15 @@ QT_MOC_LITERAL(2, 35, 0),
 QT_MOC_LITERAL(3, 36, 25),
 QT_MOC_LITERAL(4, 62, 20),
 QT_MOC_LITERAL(5, 83, 20),
-QT_MOC_LITERAL(6, 104, 21)
+QT_MOC_LITERAL(6, 104, 21),
+QT_MOC_LITERAL(7, 126, 26),
+QT_MOC_LITERAL(8, 153, 27)
     },
     "mainWindow\0on_browseButton_clicked\0\0"
     "on_MusicXMLButton_clicked\0"
     "on_MEIButton_clicked\0on_CSVButton_clicked\0"
-    "on_MIDIButton_clicked\0"
+    "on_MIDIButton_clicked\0on_MuseScoreButton_clicked\0"
+    "on_MuseScoreButton_released\0"
 };
 #undef QT_MOC_LITERAL
 
@@ -50,7 +53,7 @@ static const uint qt_meta_data_mainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+       7,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -58,13 +61,17 @@ static const uint qt_meta_data_mainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   39,    2, 0x08,
-       3,    0,   40,    2, 0x08,
-       4,    0,   41,    2, 0x08,
-       5,    0,   42,    2, 0x08,
-       6,    0,   43,    2, 0x08,
+       1,    0,   49,    2, 0x08,
+       3,    0,   50,    2, 0x08,
+       4,    0,   51,    2, 0x08,
+       5,    0,   52,    2, 0x08,
+       6,    0,   53,    2, 0x08,
+       7,    0,   54,    2, 0x08,
+       8,    0,   55,    2, 0x08,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -84,6 +91,8 @@ void mainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 2: _t->on_MEIButton_clicked(); break;
         case 3: _t->on_CSVButton_clicked(); break;
         case 4: _t->on_MIDIButton_clicked(); break;
+        case 5: _t->on_MuseScoreButton_clicked(); break;
+        case 6: _t->on_MuseScoreButton_released(); break;
         default: ;
         }
     }
@@ -115,13 +124,13 @@ int mainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 5)
+        if (_id < 7)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 7;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 5)
+        if (_id < 7)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 5;
+        _id -= 7;
     }
     return _id;
 }
